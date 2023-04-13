c1fca098102d55ef5d53533836dae0c985614a61
NhatKhoa <nhatkhoa0502@gmail.com>
2023-04-13 11:08:46 +0700

    add 404 page

---
+++ 404.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="./css/404.css">
+  </head>
+  <body>
+    <div class="container">
+      <h2>Oops! Page not found.</h2>
+      <h1>404</h1>
+      <p>We can't find the page you're looking for.</p>
+      <a href="./index.php">Go back home</a>
+    </div>
+  </body>
+</html>
